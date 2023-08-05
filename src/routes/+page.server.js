477e0832b65d2de8238a1e5cda2b776c3855f83d
havettaa <aurel.havetta@outlook.com>
2023-08-05 16:56:01 +0200

    add supabase config

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,4 @@
-import { supabase } from "$lib/supabaseClient";
+import { supabase } from "$lib/supabaseConfig";
 
 export async function load() {
 	const { data } = await supabase.from("accounts").select();
